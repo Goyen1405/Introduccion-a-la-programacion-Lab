c3ddcc6f5ef209768cfb9e60cbf232cffe0282c1
Goyen1405 <144157938+Goyen1405@users.noreply.github.com>
2023-09-05 12:26:29 -0600

    Add files via upload

---
+++ l1 Aves 2459323.py
@@ -0,0 +1,23 @@
+#Nombre del curso y sección : Introduccion a la programacion lab seccion 17 
+#Fecha de creación del programa: 05/09/2023 
+#Autor: Alejandro Echegoyen 2459323
+#Objetivo: Crear un programa en python que calcule las 4 operaciones aritmeticas basicas
+""" PROCESO
+1. Solicitar al sumarlos dos numeros enteros y realizar los resultados (Suma, resta, multiplicacion y division)
+2. mostrar resultados
+"""""
+"""" SALIDA
+3. Resultado de suma 
+4. Resultado de resta
+5. Resultado de multiplicacion
+6. resultado de division
+"""
+print("Mi primer programa")
+print("operacion con numeros")
+numero1=int(input("ingresar su numero"))
+numero2=int(input("Ingresar su numero"))
+print("suma" , numero1+numero2)
+print("resta" , numero1-numero2)
+print("multiplicacion" , numero1*numero2)
+print("division" , numero1/numero2)
+
