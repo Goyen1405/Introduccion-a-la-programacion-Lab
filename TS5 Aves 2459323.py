69013765685f6b2c23af6a0fdeeb4886c54dfdc9
Goyen1405 <144157938+Goyen1405@users.noreply.github.com>
2023-09-07 09:05:19 -0600

    Add files via upload

---
+++ TS5 Aves 2459323.py
@@ -0,0 +1,15 @@
+print("trabajo supervisado")
+print("07/09/2023")
+print("Alejandro Echegoyen")
+print("Mi segundo programa")
+sNombre= input("ingrese su nombre")
+sEdad= input("por favor ingreso su edad")
+sCarrera= input("por favor ingrese su carrera")
+sCarne= input("por favor ingresar su carne")
+
+print("nombre",sNombre)
+print("edad", sEdad)
+print("Carrera", sCarrera)
+print("carne", sCarne)
+
+print("soy" +sNombre, "tengo" +sEdad, "años y estudio la carrera de" +sCarrera, "Mi numero de carnet es: "+sCarne )
