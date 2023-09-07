eb81efbc725bdbf890dd6c173bbf882b4cfbd19b
Goyen1405 <144157938+Goyen1405@users.noreply.github.com>
2023-09-07 09:14:08 -0600

    Add files via upload

---
+++ TS5 Aves 2459323.py
@@ -0,0 +1,17 @@
+#trabajo supervisado
+#07/09/2023
+#Alejandro Echegoye
+#Mi segundo programa
+#Objetivo: Crear un programa en python que nos permita obtener informacion que se le solicita al usuario 
+
+sNombre= input("ingrese su nombre")
+sEdad= input("por favor ingreso su edad")
+sCarrera= input("por favor ingrese su carrera")
+sCarne= input("por favor ingresar su carne")
+
+print("nombre",sNombre)
+print("edad", sEdad)
+print("Carrera", sCarrera)
+print("carne", sCarne)
+
+print("soy" +sNombre, "tengo" +sEdad, "años y estudio la carrera de" +sCarrera, "Mi numero de carnet es: "+sCarne )
